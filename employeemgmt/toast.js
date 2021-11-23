bf589b52b6f2fdb5b3a3c0d99b01319883a60c62
Mananjay Shrestha <Mananjaysth111@gmail.com>
2021-11-23 17:02:35 +0545

    leaverequest-inital-form

---
+++ employeemgmt/toast.js
@@ -0,0 +1,26 @@
+// const employeelisttoast = document.querySelector(".employeelist-toast");
+
+// let icon = document.createElement("div");
+// icon.classList = "employeelist-conform-icon";
+// icon.textContent = `<i class="fas fa-check-circle"></i>`;
+
+// let toastmssg = document.createElement("div");
+// toastmssg.classList = "employeelist-toast-success-mssg";
+
+// let toastmssgTitle = document.createElement("p");
+// toastmssgTitle.textContent = `Employee`;
+// toastmssg.appendChild(toastmssgTitle);
+
+// let toastmssgSucess = document.createElement("p");
+// toastmssgSucess.textContent = `Create Successfully`;
+// toastmssg.appendChild(toastmssgSucess);
+
+// let toastclosebtn = document.createElement("button");
+// toastclosebtn.textContent = ` &times;`;
+// toastclosebtn.classList = "employeelist-toast-close-btn ";
+
+// employeelisttoast.appendChild(icon);
+// employeelisttoast.appendChild(toastmssg);
+// employeelisttoast.appendChild(toastclosebtn);
+
+// console.log(employeelisttoast);
